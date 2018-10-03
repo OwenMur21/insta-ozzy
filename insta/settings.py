4fc9e0c726cef4b7cde75f10848aa59b47a3fb38
Owenmur21 <owenmuriithi@gmail.com>
2018-10-03 23:57:35 +0300

    Adds Email configs

--- insta/settings.py
+++ insta/settings.py
@@ -58,6 +58,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'insta.urls'
 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
