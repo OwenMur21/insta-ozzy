b315d0266ea77792605d894ccb7d2cc77bb88b07
Owenmur21 <owenmuriithi@gmail.com>
2018-10-03 11:21:49 +0300

    Creates the index page template and successfully displays its contents

--- pix/views.py
+++ pix/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 def index(request):
-    return HttpResponse("This")
+    """
+    Function that renders the landing page
+    """
+    return render(request, 'index.html')
