47584251e900b9acbfc8a6a675995b65e9ef0d73
Owenmur21 <owenmuriithi@gmail.com>
2018-10-03 23:59:57 +0300

    Adds signup routes

--- pix/urls.py
+++ pix/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     url('^$', views.index, name='landing'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
