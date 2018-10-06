5194a6965621a9cf76707c7c7885c42b3b391e09
Owenmur21 <owenmuriithi@gmail.com>
2018-10-06 14:16:33 +0300

    Creates upload route

--- pix/urls.py
+++ pix/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    url(r'^upload/$', views.new_post, name='newPost'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
