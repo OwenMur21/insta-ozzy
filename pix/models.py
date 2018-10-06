ed64c39c3a3b9c5ffc25cf62647c79372e620691
Owenmur21 <owenmuriithi@gmail.com>
2018-10-06 12:17:40 +0300

    Creates functions needed for profile class

--- pix/models.py
+++ pix/models.py
@@ -2,24 +2,35 @@ from django.contrib.auth.models import User
 import datetime as dt
 from django.db import models
 from tinymce.models import HTMLField
-from pyuploadcare.dj.models import ImageField
+
 
 class Profile(models.Model):
         """
         Class that contains profile details
         """
         bio = HTMLField()
-        dp = ImageField(blank = True)
+        dp = models.ImageField(upload_to = 'images/', blank=True)
         user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
         def __str__(self):
                 return self.bio
 
+        def save_profile(self):
+                self.save()
+
+        def del_profile(self):
+                self.delete()
+
+        @classmethod
+        def search_profile(cls, user):
+                profile = cls.objects.filter(user__username__icontains=user)
+                return profile
+       
 class Image(models.Model):
         """
         Class that contains image details
         """
-        post = ImageField(blank = True, manual_crop = '1920x1080')
+        post = models.ImageField(upload_to = 'images/', blank=True)
         caption = HTMLField()
         posted_on = models.DateTimeField(auto_now_add=True)
         user = models.ForeignKey(User, on_delete=models.CASCADE)
