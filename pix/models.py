dcbc06be5086cff1a39454bc1ab6b1863d5b9c19
Owenmur21 <owenmuriithi@gmail.com>
2018-10-06 09:24:21 +0300

    Adds comment class

--- pix/models.py
+++ pix/models.py
@@ -10,18 +10,16 @@ class Profile(models.Model):
         """
         bio = HTMLField()
         dp = ImageField(blank = True)
-        user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-
+        user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
         def __str__(self):
                 return self.bio
-
 
 class Image(models.Model):
         """
         Class that contains image details
         """
-        post = ImageField( blank = True, manual_crop = '1920x1080')
+        post = ImageField(blank = True, manual_crop = '1920x1080')
         caption = HTMLField()
         posted_on = models.DateTimeField(auto_now_add=True)
         user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,6 +30,22 @@ class Image(models.Model):
 
         class Meta:
                 ordering = ['posted_on']
+
+class Comments(models.Model):
+        """
+        Class that contains comments details
+        """
+        comment = HTMLField()
+        posted_on = models.DateTimeField(auto_now=True)
+        image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments')
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+        def __str__(self):
+                return self.comment
+
+        class Meta:
+                ordering = ['posted_on']
+
 
 
  
