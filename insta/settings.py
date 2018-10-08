4417ddfd078bd6c476949b9bc5c2a866a3721121
Owenmur21 <owenmuriithi@gmail.com>
2018-10-08 19:52:09 +0300

    Fixes

--- insta/settings.py
+++ insta/settings.py
@@ -27,8 +27,7 @@ else:
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','insta-ozzy.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
